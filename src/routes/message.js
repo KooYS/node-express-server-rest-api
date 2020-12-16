c170dee0c76763cef028c00ceb7f3791073cbaff
KooYS <tlfkthsl42@naver.com>
2020-12-16 21:12:33 +0900

    Update message.js

--- src/routes/message.js
+++ src/routes/message.js
@@ -1,11 +1,29 @@
 import { v4 as uuidv4 } from 'uuid';
 import { Router } from 'express';
+import request from 'request';
 
 const router = Router();
 
 router.get('/', (req, res) => {
   return res.send(Object.values(req.context.models.messages));
 });
+
+router.get('/api_server', (req, res) => {
+  request.post(
+      'http://172.31.48.146/messages/1',
+      { json: { key: 'value' } },
+      function (error, response, body) {
+          if (!error && response.statusCode == 200) {
+              console.log(body);
+          }
+      }
+  );
+});
+
+
+
+
+
 
 router.get('/:messageId', (req, res) => {
   return res.send(req.context.models.messages[req.params.messageId]);
